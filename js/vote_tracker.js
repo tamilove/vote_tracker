5bf222fc941b119537ad8057b7669f6535c47ef1
Tami Love <thetamilove@gmail.com>
2015-07-20 15:46:25 -0700

    added a random cat player generator that works.

---
+++ js/vote_tracker.js
@@ -0,0 +1,46 @@
+var kitty = [k0, k1, k2, k3, k4, k5, k6, k7, k8, k9, k10, k11, k12, k13]
+
+kitty[i] =
+this.picture=}
+// var Tracker = function() {
+//   // this.choosing = true
+
+
+beginTurn = function() {
+  var player = [0,0];
+
+  while (player[0] == player[1]) {
+    player[0] = Math.floor(Math.random()*14);
+    player[1] = Math.floor(Math.random()*14);
+    console.log (player[0], player[1]);
+  }
+assign random player[] to a cat on kitty[]
+
+var player1 = $('#player1');
+var player2 = $('#player2');
+create player 1 picture
+create player 2 picture
+render player 1 picture to html
+render player 2 picture to html
+
+turn on listener to get clicks on pictures
+
+after click, run tracker to add to score of winning kitty
+
+set state to next battle
+
+turn on listener for button to restart beginturn
+
+}
+
+;
+
+
+
+
+
+
+
+
+
+beginTurn();
