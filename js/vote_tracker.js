24cc970ccec86f3cab438c02aaf783b968fa724b
Lyle Scott <Lyle.C.Scott@gmail.com>
2015-07-21 13:17:09 -0700

    added animations and streamlined.

--- js/vote_tracker.js
+++ js/vote_tracker.js
@@ -8,7 +8,6 @@ Tracker.prototype.Contest = function() {
   document.getElementById('player1').removeChild(document.getElementById('player1').firstChild);
   document.getElementById('player2').removeChild(document.getElementById('player2').firstChild);
   this.pickPlayers();
-  console.log("players picked", this.player[0], this.player[1], this.kitty);
   this.getVote();
 };
 
@@ -19,9 +18,9 @@ Tracker.prototype.pickPlayers = function() {
     this.player[1] = Math.floor(Math.random()*14);
   }
   leftPicBox = document.createElement('div');
-  rightPicBox = document.createElement('div');
   leftPicBox.innerHTML = "<img id=\"left\" src=\"images/" + this.player[0] + ".jpg\"/>";
   document.getElementById('player1').appendChild(leftPicBox);
+  rightPicBox = document.createElement('div');
   rightPicBox.innerHTML = "<img id=\"right\" src=\"images/" + this.player[1] + ".jpg\"/>";
   document.getElementById('player2').appendChild(rightPicBox);
 return this.player;
@@ -38,7 +37,14 @@ Tracker.prototype.getVote = function () {
     console.log ("after click R", play.kitty);
     play.Contest();
   });
+    // var cats = document.getElementById('cats').getContext('2d');
+    // new Chart(cats).Pie(playerData);
+    // var playerData = [
+    // {value: play.kitty[play.player[0]], "blue"}, 
+    // {value: play.kitty[play.player[1]], "red"}
+    // ];
 };
+
 
 var play = new Tracker();
 for (var i=0; i<14; i++) {
