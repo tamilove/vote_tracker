d7a417fdde0f8fffc6adeb9d04c6c04685e5e6b6
Tami Love <thetamilove@gmail.com>
2015-07-20 17:22:52 -0700

    working model but we need to get the kitty pictures to update after a victory.

--- js/vote_tracker.js
+++ js/vote_tracker.js
@@ -1,46 +1,66 @@
-var kitty = [k0, k1, k2, k3, k4, k5, k6, k7, k8, k9, k10, k11, k12, k13]
+//var kitty = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+var kitty = [];
+for (i=0; i<13; i++) {
+  kitty[i] = 0;
+};
+var player = [];
+var fight;
+var Tracker = function(){
 
-kitty[i] =
-this.picture=}
-// var Tracker = function() {
-//   // this.choosing = true
+};
 
+var leftVictor = function() {
+  fight = false;
+  kitty[player[0]]++ ;
+  console.log(kitty);
+  beginTurn();
+};
+
+var rightVictor = function() {
+  fight = false;
+  kitty[player[1]]++ ;
+  console.log(kitty);
+  beginTurn();
+};
 
 beginTurn = function() {
-  var player = [0,0];
+   player = [0,0];
 
   while (player[0] == player[1]) {
     player[0] = Math.floor(Math.random()*14);
     player[1] = Math.floor(Math.random()*14);
+    // player[0] = player[0].toString();
+    // player[1] = player[1].toString();
     console.log (player[0], player[1]);
   }
-assign random player[] to a cat on kitty[]
-
-var player1 = $('#player1');
-var player2 = $('#player2');
-create player 1 picture
-create player 2 picture
-render player 1 picture to html
-render player 2 picture to html
-
-turn on listener to get clicks on pictures
-
-after click, run tracker to add to score of winning kitty
-
-set state to next battle
-
-turn on listener for button to restart beginturn
-
-}
-
-;
-
-
-
-
-
-
-
-
-
+  return player [0], player[1];
+};
 beginTurn();
+
+var leftPicPlace = document.getElementById('player1');
+var leftPicBox = document.createElement('div');
+leftPicBox.innerHTML = "<img src=\"images/" + player[0] + ".jpg\"/>";
+leftPicPlace.appendChild(leftPicBox);
+leftPicPlace.addEventListener('click', leftVictor);
+
+var rightPicPlace = document.getElementById('player2');
+var rightPicBox = document.createElement('div');
+rightPicBox.innerHTML = "<img src=\"images/" + player[1] + ".jpg\"/>";
+rightPicPlace.appendChild(rightPicBox);
+rightPicPlace.addEventListener('click', rightVictor);
+
+
+
+// turn on listener to get clicks on pictures
+
+// after click, run tracker to add to score of winning kitty
+
+// set state to next battle
+
+// turn on listener for button to restart beginturn
+
+// }
+
+// ;
+
+
