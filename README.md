9df0a45e5c529baa6c3b87396da84dff4c2e000b
Tami Love <thetamilove@gmail.com>
2015-07-21 17:32:56 -0700

    added jQuery and JSON

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # vote_tracker
 cutest cat competition 
 Tami and Lyle worked together on this. 
+
+On Tuesday, we successfully added a charts.js table, fixed layout positioning, added jQuery & JSON(local storage what-not's). 
+
+Would like to still add a "refresh" button. 
--- js/vote_tracker.js
+++ js/vote_tracker.js
@@ -1,85 +1,63 @@
 var Tracker = function () {
-  this.gameOn;
   this.kitty = [];
   this.player = [0,0];
-  this.victor = "";
 };
 
-//game routine
 Tracker.prototype.Contest = function() {
-//clear scores (set up array)
-  for (i=0; i<14; i++) {
-    this.kitty[i] = 0;
-  };
-  console.log("array set up");
-  for (var i = 0; i < 14; i++) {
-    if (this.kitty[i] > 9) {
-      this.gameOn = false;
-      console.log("game ends");
-    }
-  };
-  console.log("checkpoint");
-  this.score();
-  console.log("score checked");
+  $('.cat_pic').children().remove();
   this.pickPlayers();
-  console.log("players picked");
-  this.showCats();
-  console.log("cats have been shown");
   this.getVote();
-  console.log(this.kitty);
-};
-
-//See if a cat just scored and track it
-Tracker.prototype.score = function(victor) {
-  };
-  console.log("score was checked")
-  //clear pictures if they are up
-  //console.log(this.kitty);
-  // this.leftPicPlace = document.getElementById('player1');
-  // this.leftPicBox = document.createElement('div');
-  // this.rightPicPlace = document.getElementById('player2');
-  // this.rightPicBox = document.createElement('div');
-  while (document.getElementById('player1').firstChild) {
-    document.getElementById('player1').removeChild(document.getElementById('player1').firstChild);
-  };
-  while (document.getElementById('player2').firstChild) {
-    document.getElementById('player2').removeChild(document.getElementById('player2').firstChild);
-  // }
-  // console.log(this.victor);
-  // return this.victor;
 };
 
 Tracker.prototype.pickPlayers = function() {
-//pick players
    this.player = [0,0];
   while (this.player[0] == this.player[1]) {
     this.player[0] = Math.floor(Math.random()*14);
     this.player[1] = Math.floor(Math.random()*14);
-    console.log (this.player[0], this.player[1]);
   }
+  $('#player1').append("<img id=\"left\" src=\"images/" + this.player[0] + ".jpg\"/><br><br><p>Cat number " + (this.player[0] + 1) + "</p>");
+  $('#player2').append("<img id=\"right\" src=\"images/" + this.player[1] + ".jpg\"/><br><br><p>Cat number " + (this.player[1] + 1) + "</p>");
 return this.player;
 };
 
-//Render images
-Tracker.prototype.showCats = function() {
-  // this.leftPicPlace = document.getElementById('player1');
-  this.leftPicBox = document.createElement('div');
-  // this.rightPicPlace = document.getElementById('player2');
-  this.rightPicBox = document.createElement('div');
-  this.leftPicBox.innerHTML = "<img src=\"images/" + this.player[0] + ".jpg\"/>";
-  document.getElementById('player1').appendChild(this.leftPicBox);
-  this.rightPicBox.innerHTML = "<img src=\"images/" + this.player[1] + ".jpg\"/>";
-  document.getElementById('player2').appendChild(this.rightPicBox);
-};
-
 Tracker.prototype.getVote = function () {
-  document.getElementById('player1').addEventListener('click', function() {
-    this.kitty[this.player[0]]++;
+  $('#left').on({'click': function() {
+    play.kitty[play.player[0]]++;
+    console.log ("after click L", play.kitty)
+    var cuteCatSession = JSON.stringify(play.kitty)
+    localStorage.cuteCatSession = cuteCatSession;
+    play.Contest(); }
   });
-  document.getElementById('player2').addEventListener('click', function() {
-    this.kitty[this.player[1]]++;
+  $('#right').on({'click': function() {
+    play.kitty[play.player[1]] ++;
+    console.log ("after click R", play.kitty);
+    var cuteCatSession = JSON.stringify(play.kitty)
+    localStorage.cuteCatSession = cuteCatSession;
+    play.Contest(); }
   });
+// bar chart data
+  var barData = {
+      labels : ["1","2","3","4","5","6", "7", "8", "9", "10", "11", "12", "13", "14"],
+      datasets : [
+          {
+              fillColor : "#48A497",
+              strokeColor : "#48A4D1",
+              data : play.kitty
+          },
+      ]
+  }
+  // get bar chart canvas
+  var cats = $("#cats")[0].getContext("2d");
+  // draw bar chart
+  new Chart(cats).Bar(barData);
 };
 
 var play = new Tracker();
+if (localStorage.cuteCatSession) {
+  play.kitty = JSON.parse(localStorage.cuteCatSession);
+} else {
+  for (var i=0; i<14; i++) {
+    play.kitty[i] = 0;
+  }
+};
 play.Contest();
