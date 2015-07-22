946ea04423cd4e65e4c5ed62438cfeeb328c44e5
Lyle Scott <Lyle.C.Scott@gmail.com>
2015-07-21 18:09:49 -0700

    Added reset button

--- js/vote_tracker.js
+++ js/vote_tracker.js
@@ -15,8 +15,8 @@ Tracker.prototype.pickPlayers = function() {
     this.player[0] = Math.floor(Math.random()*14);
     this.player[1] = Math.floor(Math.random()*14);
   }
-  $('#player1').append("<img id=\"left\" src=\"images/" + this.player[0] + ".jpg\"/><br><br><p>Cat number " + (this.player[0] + 1) + "</p>");
-  $('#player2').append("<img id=\"right\" src=\"images/" + this.player[1] + ".jpg\"/><br><br><p>Cat number " + (this.player[1] + 1) + "</p>");
+  $('#player1').append("<img id=\"left\" src=\"images/" + this.player[0] + ".jpg\"/><br><br><p>Cat " + (this.player[0] + 1) + "</p>");
+  $('#player2').append("<img id=\"right\" src=\"images/" + this.player[1] + ".jpg\"/><br><br><p>Cat " + (this.player[1] + 1) + "</p>");
 return this.player;
 };
 
@@ -24,18 +24,20 @@ Tracker.prototype.getVote = function () {
   $('#left').on({'click': function() {
     play.kitty[play.player[0]]++;
     console.log ("after click L", play.kitty)
-    var cuteCatSession = JSON.stringify(play.kitty)
-    localStorage.cuteCatSession = cuteCatSession;
-    play.Contest(); }
+    saveGame();
+    play.Contest();
+    }
   });
   $('#right').on({'click': function() {
     play.kitty[play.player[1]] ++;
     console.log ("after click R", play.kitty);
-    var cuteCatSession = JSON.stringify(play.kitty)
-    localStorage.cuteCatSession = cuteCatSession;
-    play.Contest(); }
+    saveGame();
+    play.Contest();
+    }
   });
-// bar chart data
+};
+
+var renderChart = function() {
   var barData = {
       labels : ["1","2","3","4","5","6", "7", "8", "9", "10", "11", "12", "13", "14"],
       datasets : [
@@ -52,7 +54,20 @@ Tracker.prototype.getVote = function () {
   new Chart(cats).Bar(barData);
 };
 
+var saveGame = function() {
+  var cuteCatSession = JSON.stringify(play.kitty)
+  localStorage.cuteCatSession = cuteCatSession;
+  renderChart();
+};
+
 var play = new Tracker();
+$('#reset').on({'click': function() {
+      for (var i=0; i<14; i++) {
+      play.kitty[i] = 0;
+    }
+    saveGame();
+  }
+});
 if (localStorage.cuteCatSession) {
   play.kitty = JSON.parse(localStorage.cuteCatSession);
 } else {
@@ -60,4 +75,5 @@ if (localStorage.cuteCatSession) {
     play.kitty[i] = 0;
   }
 };
+renderChart();
 play.Contest();
