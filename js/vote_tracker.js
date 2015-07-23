8015ef83b3a9031a0f6392f176c1b9330589fcd4
Tami Love <thetamilove@gmail.com>
2015-07-23 15:48:46 -0700

    got pictures to show, still not able to get the chart to work properly.

--- js/vote_tracker.js
+++ js/vote_tracker.js
@@ -1,3 +1,20 @@
+var pics;
+
+$.ajax({
+  url: 'https://api.imgur.com/3/album/VAOvY/json',
+  method: 'GET',
+  headers: {
+    'Authorization': 'Client-ID fabac30d185a0c6'
+  }
+}).done(function(res) {
+  pics = res.data.images;
+  console.log(pics);
+  showFromImgur();
+  play.Contest();
+}).fail(function(err) {
+  console.log(err);
+});
+
 var Tracker = function () {
   this.kitty = [];
   this.player = [0,0];
@@ -5,54 +22,77 @@ var Tracker = function () {
 
 Tracker.prototype.Contest = function() {
   $('.cat_pic').children().remove();
-  this.pickPlayers();
+  showFromImgur();
   this.getVote();
 };
 
-Tracker.prototype.pickPlayers = function() {
-   this.player = [0,0];
-  while (this.player[0] == this.player[1]) {
-    this.player[0] = Math.floor(Math.random()*14);
-    this.player[1] = Math.floor(Math.random()*14);
+function showFromImgur() {
+  randL = 0;
+  randR = 0;
+  while (randL == randR) {
+    var randL = Math.floor(Math.random() * pics.length);
+    console.log(pics);
+    var randR = Math.floor(Math.random() * pics.length);
+    console.log(pics);
+    var displayPicL = '<img id=\"left\" src=\"' + pics[randL].link + '\"/>';
+    var displayPicR = '<img id=\"right\" src=\"' + pics[randR].link + '\"/>';
+    $('#player1').html(displayPicL);
+    $('#player2').html(displayPicR);
   }
-  $('#player1').append("<img id=\"left\" src=\"images/" + this.player[0] + ".jpg\"/><br><br><p>Cat number " + (this.player[0] + 1) + "</p>");
-  $('#player2').append("<img id=\"right\" src=\"images/" + this.player[1] + ".jpg\"/><br><br><p>Cat number " + (this.player[1] + 1) + "</p>");
-return this.player;
-};
+}
+
+$ ('#button').on({'click': function(){
+  showFromImgur();
+  }
+});
 
 Tracker.prototype.getVote = function () {
   $('#left').on({'click': function() {
     play.kitty[play.player[0]]++;
     console.log ("after click L", play.kitty)
-    var cuteCatSession = JSON.stringify(play.kitty)
-    localStorage.cuteCatSession = cuteCatSession;
-    play.Contest(); }
+    saveGame();
+    play.Contest();
+    }
   });
   $('#right').on({'click': function() {
     play.kitty[play.player[1]] ++;
     console.log ("after click R", play.kitty);
-    var cuteCatSession = JSON.stringify(play.kitty)
-    localStorage.cuteCatSession = cuteCatSession;
-    play.Contest(); }
+    saveGame();
+    play.Contest();
+    }
   });
-// bar chart data
+};
+
+var renderChart = function() {
   var barData = {
       labels : ["1","2","3","4","5","6", "7", "8", "9", "10", "11", "12", "13", "14"],
       datasets : [
           {
               fillColor : "#48A497",
               strokeColor : "#48A4D1",
-              data : play.kitty
+              data : 'play.kitty'
           },
       ]
   }
-  // get bar chart canvas
+  //bar chart from Charts.js
   var cats = $("#cats")[0].getContext("2d");
-  // draw bar chart
   new Chart(cats).Bar(barData);
 };
 
+var saveGame = function() {
+  var cuteCatSession = JSON.stringify(play.kitty)
+  localStorage.cuteCatSession = cuteCatSession;
+  renderChart();
+};
+
 var play = new Tracker();
+$('#reset').on({'click': function() {
+      for (var i=0; i<14; i++) {
+      play.kitty[i] = 0;
+    }
+    saveGame();
+  }
+});
 if (localStorage.cuteCatSession) {
   play.kitty = JSON.parse(localStorage.cuteCatSession);
 } else {
@@ -60,4 +100,7 @@ if (localStorage.cuteCatSession) {
     play.kitty[i] = 0;
   }
 };
-play.Contest();
+
+renderChart();
+
+
